bda791cee6763236914772d6c97274a8997bd128
Pmarc21 <pablo.marc.barrera@gmail.com>
2022-03-29 18:18:44 +0200

    actualización

--- weatherapp/app.py
+++ weatherapp/app.py
@@ -1,5 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+
+posts = []
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return '<h1>Hola World 222!<h1>'
+    return render_template("index.html", num_posts=len(posts))
+
+@app.route("/p/<string:slug>/")
+def show_post(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
